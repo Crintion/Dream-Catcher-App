281060c1459e146ebca6d8ced667d1343a6da761
Christian Spencer <64983017+Crintion@users.noreply.github.com>
2021-08-14 10:15:09 -0700

    changed the journal_id to calendar_id for clarity and added in a new route to get all calendar id's

--- app/models/journal_entry.py
+++ app/models/journal_entry.py
@@ -3,18 +3,14 @@ from app import db
 
 class JournalEntry(db.Model):
     journal_entry_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    journal_month = db.Column(db.Integer, nullable = False)
-    journal_day = db.Column(db.Integer, nullable = False)
-    journal_title = db.Column(db.String, nullable = False)
+    calendar_id = db.Column(db.Integer, nullable = False)
     journal_text = db.Column(db.String, nullable = False)
     journal_mood = db.Column(db.String, nullable = False)
 
     def to_json(self):
         
         return {
-            "journal_month" : self.journal_month,
-            "journal_day" : self.journal_day,
-            "journal_title" : self.journal_title,
+            "calendar_id": self.calendar_id,
             "journal_text" : self.journal_text,
             "journal_mood" : self.journal_mood
         }
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from app import db
 from app.models.journal_entry import JournalEntry
 from flask import Blueprint, request, make_response, jsonify
-from datetime import datetime
 import requests
 import flask_migrate
 import os
@@ -13,63 +12,62 @@ journal_entry_bp = Blueprint("journal_entry", __name__, url_prefix='/journal_ent
 @journal_entry_bp.route("", methods=["POST", "PUT"])
 def post_entry():
     request_body = request.get_json()
-    new_entry = JournalEntry(
-        journal_month=request_body["journal_month"],
-        journal_day=request_body["journal_day"], 
-        journal_title=request_body["journal_title"],
+    new_entry = JournalEntry( 
+        calendar_id=request_body["calendar_id"],
         journal_text=request_body["journal_text"],
         journal_mood = request_body["journal_mood"])
-    entry = JournalEntry.query.filter_by(journal_month = request_body["journal_month"], journal_day = request_body["journal_day"]).first()
+    entry = JournalEntry.query.filter_by(calendar_id = request_body["calendar_id"]).first()
     if not entry:
         db.session.add(new_entry)
         db.session.commit()
-        return make_response(f"Journal: {new_entry.journal_title} was successfully created"), 201
+        return make_response(f"Journal: {new_entry.calendar_id} was successfully created"), 201
     else:
-        entry.journal_title=request_body["journal_title"]
+        entry.calendar_id=request_body["calendar_id"]
         entry.journal_text=request_body["journal_text"]
         entry.journal_mood=request_body["journal_mood"]
 
         db.session.commit()
         
-    return make_response(f"Journal: {new_entry.journal_title} was successfully updated"), 200
+    return make_response(f"Journal: {new_entry.calendar_id} was successfully updated"), 200
 
 @journal_entry_bp.route("", methods=["GET"])
 def get_all_entries():
     journal_entry_response_body = []
+    print(journal_entry_response_body)
     journal_entries = JournalEntry.query.all()
+    print(journal_entries)
     for entry in journal_entries:
         journal_entry_response_body.append(entry.to_json())
+        print(journal_entry_response_body)
     return jsonify(journal_entry_response_body), 200
+
+@journal_entry_bp.route("/calendar_ids", methods=["GET"])
+def get_all_calendar_ids():
+    calendar_id_response_body = []
+    journal_entries = JournalEntry.query.all()
+    for entry in journal_entries:
+        calendar_id_response_body.append(entry.calendar_id)
+    return jsonify(calendar_id_response_body), 200
+
+
     
-@journal_entry_bp.route("/<journal_entry_id>", methods=["GET"])
+@journal_entry_bp.route("/journal_entry_id/<journal_entry_id>", methods=["GET"])
 def get_entry_by_id(journal_entry_id):
     entry = JournalEntry.query.get(journal_entry_id)
     if not entry:
         return "", 404
     return make_response({"journal_entry": entry.to_json()}), 200
 
-@journal_entry_bp.route("/month/<journal_month>/day/<journal_day>", methods=["GET"])
-def get_entry_by_date(journal_month, journal_day):
-    entry = JournalEntry.query.filter_by(journal_month = journal_month, journal_day = journal_day).first()
+
+@journal_entry_bp.route("/calendar_id/<calendar_id>", methods=["GET"])
+def get_entry_by_calendar_id(calendar_id):
+    entry = JournalEntry.query.get(calendar_id)
     if not entry:
         return "", 404
     return make_response({"journal_entry": entry.to_json()}), 200
+    
 
-@journal_entry_bp.route("/month/<journal_month>/day/<journal_day>", methods=["DELETE"])
-def delete_entry(journal_month, journal_day):
-    entry = JournalEntry.query.filter_by(journal_month = journal_month, journal_day = journal_day).first()
-    if not entry:
-        return "", 404
-    else:
-        db.session.delete(entry)
-        db.session.commit()
-    return make_response("Journal entry deleted"), 200
 
-# @journal_entry_bp.after_request # solves cors
-# def after_request(response):
-#     header = response.headers
-#     header['Access-Control-Allow-Origin'] = '*'
-#     return response
             
 
 
